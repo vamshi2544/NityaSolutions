0d0a40ab959b5fef363f990ca5152a0a4f7d1fee
vamshi2544 <vamshi2544@gmail.com>
2014-06-10 12:13:24 -0700

    First commit in nityasolutions

---
+++ src/NityaSolutions/FirstClassProg.java
@@ -0,0 +1,19 @@
+package NityaSolutions;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+ 
+public class FirstClassProg {
+ 
+    public static void main(String a[]){
+        ArrayList<String> arrl = new ArrayList<String>();
+        arrl.add("First");
+        arrl.add("Second");
+        arrl.add("Third");
+        arrl.add("Random");
+        Iterator<String> itr = arrl.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
+}
