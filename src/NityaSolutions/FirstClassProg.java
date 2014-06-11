945df69d581a2b22ccb5d06cbb1420e0e7a2d660
yari <yariyakup@gmail.com>
2014-06-11 00:08:09 -0400

    Added Comment on the class

--- src/NityaSolutions/FirstClassProg.java
+++ src/NityaSolutions/FirstClassProg.java
@@ -2,9 +2,22 @@ package NityaSolutions;
 
 import java.util.ArrayList;
 import java.util.Iterator;
- 
+/**
+ *  Explain this class what this for
+ *  
+ * @author put your name here
+ */
 public class FirstClassProg {
- 
+	/**
+	 * put explanation here. 
+	 */
+	private String field1;
+    /**
+     * explain what ever method u wrote 
+     * and what method is do 
+     * 
+     * @param a  explain this Param
+     */
     public static void main(String a[]){
         ArrayList<String> arrl = new ArrayList<String>();
         arrl.add("First");
@@ -15,5 +28,14 @@ public class FirstClassProg {
         while(itr.hasNext()){
             System.out.println(itr.next());
         }
+    }
+    /**
+     * put explanation of this method  here
+     * 
+     * @return int explain what this method will return
+     */
+    public static int getCount() {
+    	
+    	return 0;
     }
 }
