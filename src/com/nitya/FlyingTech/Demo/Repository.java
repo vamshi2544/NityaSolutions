da9996c5b9222153694b610d03d3bca5ed7e58f4
yari <yariyakup@gmail.com>
2014-06-17 19:11:15 -0400

    Update Repository.

--- src/com/nitya/FlyingTech/Demo/Repository.java
+++ src/com/nitya/FlyingTech/Demo/Repository.java
@@ -11,57 +11,59 @@ public class Repository {
 	/**
 	 * 
 	 */
-   public static ArrayList<Employee> employeeList = new ArrayList<Employee>();
-   
-    static Employee emp1 = new Employee("vamshi",1000,10000);
-   static Employee emp2 = new Employee("nikhil",1001,10002);
+	private static ArrayList<Employee> employeeList = new ArrayList<Employee>();
 
- static Employee emp3=new Employee("sai",1002,5000);
- static Employee emp4=new Employee("shiva",1003,5000);  
- static Employee emp5=new Employee("sai kumar",1004,5000);
- static Employee emp6=new Employee("nithin",1005,5000);
- static Employee emp7=new Employee("kalyan",1006,5000);
- static Employee emp8=new Employee("tapan",1007,5000);
- 
-   /**
-    * 
-    * @param empId
-    * @return
-    */
+	/**
+	 * 
+	 * @param empId
+	 * @return
+	 */
 	public static Employee getEmployee(int empId) {
-		employeeList = generateEmployee();
+	//using for loop, loop throw the employeeList if employee id is equal to empId, then return that employee
+
 		return emp1;
 	}
-	
-    /**
-     * 
-     * @param firstName
-     * @return
-     */
+
+	/**
+	 * 
+	 * @param firstName
+	 * @return
+	 */
 	public static Employee getEmployee(String firstName) {
-		return firstName;		
-		
+		//using for loop, loop throw the employee  if employee firstname euqal to firstName then return the employee
+
+		return firstName;
+
 	}
-    /**
-     * 
-     * @param lastName
-     * @return
-     */
+
+	/**
+	 * 
+	 * @param lastName
+	 * @return
+	 */
 	public static Employee getEmployee(String lastName) {
 		return lastName;
-        
+
 	}
+
 	/**
 	 * 
 	 * @return
 	 */
 	private static ArrayList<Employee> generateEmployee() {
-		ArrayList<Employee> empList = new ArrayList<Employee>();
 		/**
 		 * generate your employee and return
 		 */
-	
-		employeeList.add(emp1);	
+		Employee emp1 = new Employee("vamshi", 1000, 10000);
+		Employee emp2 = new Employee("nikhil", 1001, 10002);
+		Employee emp3 = new Employee("sai", 1002, 5000);
+		Employee emp4 = new Employee("shiva", 1003, 5000);
+		Employee emp5 = new Employee("sai kumar", 1004, 5000);
+		Employee emp6 = new Employee("nithin", 1005, 5000);
+		Employee emp7 = new Employee("kalyan", 1006, 5000);
+		Employee emp8 = new Employee("tapan", 1007, 5000);
+
+		employeeList.add(emp1);
 		employeeList.add(emp2);
 		employeeList.add(emp3);
 		employeeList.add(emp4);
@@ -71,12 +73,10 @@ public class Repository {
 		employeeList.add(emp8);
 		return employeeList;
 
-			
 	}
 
-
-public static void main(String args[]) {
-Repository	r = new Repository();
-System.out.println(r);
-}
+	public static void main(String args[]) {
+		Repository r = new Repository();
+		System.out.println(r);
+	}
 }
