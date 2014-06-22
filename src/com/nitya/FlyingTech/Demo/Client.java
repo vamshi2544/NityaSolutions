3a08d94211a731ae49e5cf63cab4f2870fee36e1
vamshi2544 <vamshi2544@gmail.com>
2014-06-21 17:05:59 -0700

    Revert "Finished project"
    This reverts commit 83f495212278fe440060e6c502ae1850028f0661.

--- src/com/nitya/FlyingTech/Demo/Client.java
+++ src/com/nitya/FlyingTech/Demo/Client.java
@@ -1,7 +1,5 @@
 package com.nitya.FlyingTech.Demo;
 
-import java.util.Scanner;
-
 /**
  * 
  * This class is client class Used to call employee class and give details of
@@ -18,6 +16,7 @@ public class Client {
 	 *            - can give any number of arguements here
 	 */
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		/**
 		 * just invoking new objects with method Employee_repo
 		 * 
@@ -26,13 +25,20 @@ public class Client {
 		 * @see emp3 object
 		 * @see emp4 object
 		 */
-		// EmployeeRepo emp1 = new EmployeeRepo();
-		// EmployeeRepo emp2 = new EmployeeRepo("vivek");
-		// emp2.setEmp_id(1002);
-		// EmployeeRepo emp3 = new EmployeeRepo("pradeep", 1003);
-		// emp3.setSalary(6000);
-		// EmployeeRepo emp4 = new EmployeeRepo("ramu", 1004, 8000);
-		// Employee emp = Repository.getEmployee(1000);
+
+
+//		EmployeeRepo emp1 = new EmployeeRepo();
+//		EmployeeRepo emp2 = new EmployeeRepo("vivek");
+//		emp2.setEmp_id(1002);
+//		EmployeeRepo emp3 = new EmployeeRepo("pradeep", 1003);
+//		emp3.setSalary(6000);
+//		EmployeeRepo emp4 = new EmployeeRepo("ramu", 1004, 8000);
+//		Employee emp = Repository.getEmployee(1000);
+		
+		
+		System.out.println(emp.toString());
+	
+
 
 		/**
 		 * prints firstname , emp_id, salary of emp1 object
@@ -41,9 +47,9 @@ public class Client {
 		 * @see emp1.getEmp_id
 		 * @see emp1.getsalary
 		 */
-		// System.out.println("Firstname is " + emp1.getFirstname()
-		// + "  emp_id is " + emp1.getEmp_id() + "  salary is "
-		// + emp1.getSalary());
+//		System.out.println("Firstname is " + emp1.getFirstname()
+//				+ "  emp_id is " + emp1.getEmp_id() + "  salary is "
+//				+ emp1.getSalary());
 		/**
 		 * prints firstname , emp_id, salary of emp2 object
 		 * 
@@ -51,9 +57,9 @@ public class Client {
 		 * @see emp2.getEmp_id
 		 * @see emp2.getsalary
 		 */
-		// System.out.println("Firstname is " + emp2.getFirstname()
-		// + "  emp_id is " + emp2.getEmp_id() + "  salary is "
-		// + emp2.getSalary());
+//		System.out.println("Firstname is " + emp2.getFirstname()
+//				+ "  emp_id is " + emp2.getEmp_id() + "  salary is "
+//				+ emp2.getSalary());
 		/**
 		 * prints firstname , emp_id, salary of emp3 object
 		 * 
@@ -61,9 +67,9 @@ public class Client {
 		 * @see emp3.getEmp_id
 		 * @see emp3.getsalary
 		 */
-		// System.out.println("Firstname is " + emp3.getFirstname()
-		// + "  emp_id is " + emp3.getEmp_id() + "  salary is "
-		// + emp3.getSalary());
+//		System.out.println("Firstname is " + emp3.getFirstname()
+//				+ "  emp_id is " + emp3.getEmp_id() + "  salary is "
+//				+ emp3.getSalary());
 		/**
 		 * prints firstname , emp_id, salary of emp4 object
 		 * 
@@ -71,32 +77,13 @@ public class Client {
 		 * @see emp4.getEmp_id
 		 * @see emp4.getsalary
 		 */
-		// System.out.println("Firstname is " + emp4.getFirstname()
-		// + "  emp_id is " + emp4.getEmp_id() + "  salary is "
-		// + emp4.getSalary());
+//		System.out.println("Firstname is " + emp4.getFirstname()
+//				+ "  emp_id is " + emp4.getEmp_id() + "  salary is "
+//				+ emp4.getSalary());
 
-		System.out.println(Repository.getEmployee("vamshi"));
-		System.out.println(Repository.getEmployee(1001));
-		System.out.println(Repository.getEmployee(5000));
-
-		/**
-		 * YARI HERE AM TRIED TO GIVE INPUT BUT AM GETTING ERROR JUS CHECK
-		 * STRING b IN THE ELSE STATEMENT 
-		 * 
-		 * Scanner in = new Scanner(System.in);
-		 * System.out.println("Enter employee's empid or firtname:");
-		 * while(in.hasNext()) 
-		 * 		{ 
-		 * 		if (in.hasNextInt()) 
-		 * 			{ 
-		 * 			int a= in.nextInt();
-		 * 			System.out.println(Repository.getEmployee(a)); 
-		 * 			} 
-		 * 		else {
-		 * 		b = in.next();
-		 * System.out.println(Repository.getEmployee(b)); 
-		 * 			}
-		 */
-
+		Repository.getEmployee(1001);
+		
+		
 	}
+
 }
--- src/com/nitya/FlyingTech/Demo/Employee.java
+++ src/com/nitya/FlyingTech/Demo/Employee.java
@@ -1,104 +1,55 @@
 
 package com.nitya.FlyingTech.Demo;
 /**
- * This class name is  Employee and used to jus define getter ,setter methods
- * @author vamshikrishna
+ * 
+ * @author yari_uygur
  *
  */
 public class Employee {
 	/**
-	 * @see firstName to store firstname of an employee
+	 * explain field
 	 */
 	private String firstName;
 	/**
-	 * @see empId To store id of an employee
+	 * explain field
 	 */
 	private int empId;
 	/**
-	 *@see salary To store salary of an employee
+	 * explain field
 	 */
 	private float salary;
-	/**
-	 * @see lastName To store lastname of an employee
-	 */
-	private String lastName;
-/**
- * this is constructor with four paramters i.e details of employee
- * @param empId1 
- * @param firstName1
- * @param lastName1
- * @param salary1
- */
-	public  Employee(int empId1,String firstName1,String lastName1,int salary1){
+	
+	public Employee(){	
+	}
+	public  Employee(String firstName1,int empId1,float salary1){
 		firstName=firstName1;
-		lastName=lastName1;
 		empId=empId1;
 		salary=salary1;
 	}
-	/**
-	 * Getter method for empid to take id of employee
-	 * @return empid
-	 */
-	public int getEmpId() {
-		return empId;
-	}
-	/**
-	 * setter method for empid
-	 * @param empId
-	 */
-	public void setEmpId(int empId) {
-		this.empId = empId;
-	}
-	/**
-	 * getter method for firstname
-	 * @return firstname 
-	 */
+	
 	public String getFirstName() {
 		return firstName;
 	}
-	/**
-	 * setter method for firstname of an employee
-	 * @param firstName
-	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	/**
-	 * getter method for lastname
-	 * @return lastname
-	 */
-	public String getLastName() {
-		return lastName;
+	public int getEmpId() {
+		return empId;
 	}
-	/**
-	 * setter method for lastname to store string
-	 * @param lastName
-	 */
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setEmpId(int empId) {
+		this.empId = empId;
 	}
-	/**
-	 * getter method for salary
-	 * @return salary
-	 */
 	public float getSalary() {
 		return salary;
 	}
-	/**
-	 * setter method for salary to set salary for employee
-	 * @param salary
-	 */
 	public void setSalary(float salary) {
 		this.salary = salary;
 	}
 	@Override
 	public String toString() {
-//		return "Employee [firstName=" + firstName + ", empId=" + empId
-//				+ ", salary=" + salary + "]";
-		return "Employee [ Employeeid is "+empId+",Firstname is "+firstName+""
-				+ ", Lastname is "+lastName+", and salary = "+salary+" ]";
+		return "Employee [firstName=" + firstName + ", empId=" + empId
+				+ ", salary=" + salary + "]";
 	}
-	
 	
 }
 
